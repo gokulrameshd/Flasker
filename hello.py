bc350b621998391925f8e583e4827906fd060a71
Gokulramesh Dhanasekar <gorad364@gmail.com>
2021-08-21 22:14:30 +0530

    second commit

--- hello.py
+++ hello.py
@@ -1,8 +1,34 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #create a flask instance
 app = Flask(__name__)
+
+# CSFR 
+app.config['SECRET_KEY'] = "my super secret key that no one supposed to know"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name" , validators = [DataRequired()])
+    submit = SubmitField("Submit")
+    # BooleanField
+    # DateField
+    # DecimalField
+    # FileField
+    # HiddenField
+    # MultipleField
+    # FloatField
+    # FromField
+    # IntegerField
+    # PasswordField
+    # RadioField
+    # URL
+    # UUID
+    # IP
+
+
 
 # Create a route decorator
 @app.route("/")
@@ -47,3 +73,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name Page
+@app.route("/name",methods = ['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form submitted successfully!!")
+    return render_template("name.html",
+                            name = name,
+                            form = form)
